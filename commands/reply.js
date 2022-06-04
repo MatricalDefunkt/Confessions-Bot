7067952ddbdf96533309688ce0aea02354052d97
MatricalDefunkt <pramitsmusic@gmail.com>
2022-06-05 00:36:51 +0530

    Integrated new sqlite database. Added functionality to confirm whether a user wants to send the confession or not.

--- commands/reply.js
+++ commands/reply.js
@@ -1,127 +1,150 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
-const { MessageEmbed } = require('discord.js');
-const { airtable_API } = require('../config.json');
-const { airtableBase } = require('../config.json');
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: `${airtable_API}`}).base(airtableBase);
+const { SlashCommandBuilder } = require( '@discordjs/builders' )
+const { MessageEmbed, CommandInteraction, Client, MessageActionRow, MessageButton, Message } = require( 'discord.js' );
+const { BlockLogs, Confessions } = require( '../database/database' );
+const { confessId, guildId } = require( '../config.json' );
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('confessreply')
-        .setDescription('Input an appropriate link, or message ID, and reply to the message using the bot!')
-        .addStringOption(o => o
-			.setName(`type`)
-			.setDescription(`Please provide a type of confession:`)
-			.setRequired(true)
-			.addChoice(`Anonymous`, `1`)
-			.addChoice(`Signed`, `2`)
-			)
-        .addStringOption(o => o
-            .setName('reply')
-            .setDescription('The reply you would like to give to the confession.')
-            .setRequired(true)
-            )
-        .addStringOption(o => o
-            .setName('link-or-id')
-            .setDescription('A link (or an ID) of the message you would like to reply to')
-            .setRequired(true)
+        .setName( 'confessreply' )
+        .setDescription( 'Input an appropriate link, or message ID, and reply to the message using the bot!' )
+        .addStringOption( o => o
+            .setName( `type` )
+            .setDescription( `Please provide a type of confession:` )
+            .setRequired( true )
+            .addChoice( `Anonymous`, `1` )
+            .addChoice( `Signed`, `2` )
+        )
+        .addStringOption( o => o
+            .setName( 'reply' )
+            .setDescription( 'The reply you would like to give to the confession.' )
+            .setRequired( true )
+        )
+        .addStringOption( o => o
+            .setName( 'link-or-id' )
+            .setDescription( 'A link (or an ID) of the message you would like to reply to' )
+            .setRequired( true )
         ),
-    async execute(interaction, client) {
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute ( interaction, client )
+    {
+        if ( !interaction.inCachedGuild() ) return
 
-        base('Blocks').select({
-			filterByFormula: `BlkID = ${interaction.user.id}`	
-		}).eachPage(function page(records, fetchNextPage) {
+        const confessChannel = await client.channels.fetch( confessId, { force: false, cache: true } )
+        if ( !confessChannel.isText() ) return
 
-                try{
-                    if (records[0]) {return interaction.editReply({content: `Sorry, but you have been blocked by staff from using this bot. Don't worry, nobody knows anything about the confession which you made, your privacy has been ensured.\nPlease go to <#812700884256686110> and create a ticket to get help.`})}
-                    fetchNextPage();
-                } catch (err) {
-                    console.error(err)
-                }
-            }, async function done(error){
+        const blockedUser = await BlockLogs.findOne( { where: { usrID: interaction.user.id } } );
+        const action = blockedUser?.getDataValue( "action" );
 
-            const linkOrId = interaction.options.getString('link-or-id')
-            const baseLink = 'https://discord.com/channels/793202043703001098/955300269162385418'
+        const replyText = interaction.options.getString( "reply", true )
 
-            if (linkOrId.startsWith(baseLink)) {
-                
-                const msgId = linkOrId.substring(baseLink.length)
+        if ( action === "unblock" || !action )
+        {
 
-                try {
+            const linkOrId = interaction.options.getString( "link-or-id" )
+            let link;
+            let replyId;
 
-                    const channel = await interaction.guild.channels.fetch("955300269162385418");
-                    const confession = await channel.messages.fetch(msgId);
-                    if (confession.author !== client.user) return interaction.editReply({content: `The author of this message is not ${client.user}, so I cannot reply to it.`});
-
-                    createdNewReply(confession, interaction, client);
-
-                } catch (error) {
-
-                    console.error(error)
-                    interaction.editReply({content: `There was an error. It could be that the link was invalid.`})
-
-                }
-            } else if (BigInt(linkOrId)) {
-
-                try {
-                    
-                    const channel = await interaction.guild.channels.fetch("955300269162385418");
-                    const confession = await channel.messages.fetch(linkOrId);
-                    if (confession.author !== client.user) return interaction.editReply({content: `The author of this message is not ${client.user}, so I cannot reply to it.`});
-
-                    createdNewReply(confession, interaction, client);
-
-                } catch (error) {
-                    
-                    console.error(error)
-                    interaction.editReply({ content: `Please input a valid 'link-or-id'. Here is your reply content:\n${interaction.options.getString('reply')}`})
-
-                }
-            } else {
-                interaction.editReply({ content: `Please input a valid 'link-or-id'. Here is your reply content:\n${interaction.options.getString('reply')}`})
-            }
-		})
-    }       
-}
-
-const createdNewReply = (confession, interaction, client) => {
-    const confType = interaction.options.getString('type');
-    const reply = interaction.options.getString('reply');
-    const icon = interaction.user.displayAvatarURL({dynamic: true});
-
-    let embed = new MessageEmbed()
-        .setColor('RANDOM')
-        .setDescription(`\n\n${reply}\n`)
-        .setTimestamp()
-        .setTitle('Reply:');
-
-    if (confType === '1') {
-
-        embed.setAuthor({name: `Anon#0000`, iconURL: `${client.user.avatarURL()}`});
-        confession.reply({embeds: [embed]}).then(msg => newReply(msg));
-        return interaction.editReply({content: `Your reply has been sent! Please open a ticket in <#812700884256686110> if there are any issues!`})
-
-    } else if (confType === '2') {
-
-        embed.setAuthor({name: `${interaction.user.tag}`, iconURL: `${icon}`}).setFooter({text: `Please note that even though this confession was not anonymous, it does not allow anyone to send nasty stuff to ${interaction.user.username}. Read rule#2.`});
-        confession.reply({embeds: [embed]}).then(msg => newReply(msg));
-        return interaction.editReply({content: `Your confession has been sent! Please open a ticket in <#812700884256686110> if there are any issues!`});
-
-    }
-
-    async function newReply(msg){
-        base('Confessions').create([
+            if ( linkOrId.length === 18 )
             {
-            "fields": {
-                "UsrID": `${interaction.user.id}`,
-                "MsgID": `${msg.id}`
+                replyId = linkOrId;
+                link = `https://discord.com/channels/${ guildId }/${ confessId }/${ linkOrId }`;
+            } else
+            {
+                replyId = linkOrId.substring( 67, 85 )
+                link = linkOrId
             }
+
+            if ( link.length !== 85 || replyId.length !== 18 ) return interaction.editReply( { content: `Please check the link or ID you have provided. Here is your reply for your reference:\n----------------\n${ replyText }` } )
+
+            const embed = new MessageEmbed()
+                .setColor( 'RANDOM' )
+                .setDescription( `\n\n${ replyText }\n` )
+                .setTimestamp()
+                .setTitle( 'Reply:' );
+
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId( 'sendconfirm' )
+                        .setEmoji( '✅' )
+                        .setStyle( 'SUCCESS' )
+                        .setLabel( 'Send' ),
+                    new MessageButton()
+                        .setCustomId( 'sendcancel' )
+                        .setEmoji( '❎' )
+                        .setStyle( 'PRIMARY' )
+                        .setLabel( 'Cancel' )
+                )
+
+            const filter = ( button ) => button.user.id === interaction.user.id
+            const reply = await interaction.editReply( { content: `You are about to reply to [this message](${ link }). Send it? You have 60 seconds to decide.`, embeds: [ embed ], components: [ row ] } )
+            reply.awaitMessageComponent( {
+                filter,
+                time: 60000,
+                componentType: 'BUTTON'
+            } ).then( async ( collected ) =>
+            {
+                const confType = interaction.options.getString( "type", true )
+                if ( collected.customId === 'sendconfirm' )
+                {
+                    try
+                    {
+                        const confession = await confessChannel.messages.fetch( replyId, { force: false, cache: true } )
+                        if ( !confession ) return interaction.editReply( { content: `Please check the message link or ID you have provided.` } )
+
+                        if ( confType === '1' )
+                        {
+                            embed.setAuthor( { name: `Anon#0000`, iconURL: `${ client.user.avatarURL() }` } );
+                            confession.reply( { embeds: [ embed ] } ).then( msg => newReply( msg ) );
+                            return collected.update( {
+                                content: `Your reply has been sent! Please open a ticket in <#812700884256686110> if there are any issues!\n
+													    We hope that this helps you in feeling better!`, embeds: [], components: []
+                            } )
+                        } else if ( confType === '2' )
+                        {
+                            embed.setAuthor( { name: `${ interaction.user.tag }`, iconURL: `${ interaction.user.displayAvatarURL( { dynamic: true } ) }` } ).setFooter( { text: `Please note that even though this reply was not anonymous, it does not allow anyone to send nasty stuff to ${ interaction.user.username }. Read rule#2.` } );
+                            confession.reply( { embeds: [ embed ] } ).then( msg => newReply( msg ) );
+                            return collected.update( {
+                                content: `Your reply has been sent! Please open a ticket in <#812700884256686110> if there are any issues.\n
+													    We hope that this helps you in feeling better!`, embeds: [], components: []
+                            } );
+                        }
+                    } catch ( error )
+                    {
+                        if ( error.code === 10008 ) return collected.update( { content: `The message is not from the confessions channel.`, embeds: [], components: [] } )
+                        else console.error( error )
+                    }
+                } else
+                {
+                    return collected.update( { content: `The reply was not sent. For your reference, here is your reply:\n----------------\n${ replyText }`, embeds: [], components: [] } )
+                }
+            } ).catch( async ( err ) =>
+            {
+                console.error( err )
+                interaction.editReply( { content: `You did not click the buttons in time. If you wish to try again, here is your reply:\n----------------\n${ replyText }`, embeds: [], components: [] } )
+            } )
+
+            /**
+             * 
+             * @param {Message} msg 
+             */
+            async function newReply ( msg )
+            {
+                Confessions.create( {
+                    usrID: interaction.user.id,
+                    msgID: msg.id
+                } ).then( () => { } ).catch( ( err ) =>
+                {
+                    console.error( err )
+                } );
             }
-        ], function(err, records) {
-            if (err) {
-            console.error(err);
-            return;
-            }
-        })
+        } else if ( action === "block" )
+        {
+            return interaction.editReply( { content: `Sorry, but you have been blocked by staff from using this bot. Don't worry, nobody knows anything about the confession which you made, your privacy has been ensured.\nPlease go to <#812700884256686110> and create a ticket to get help.` } )
+        }
     }
 }
--- commands/send.js
+++ commands/send.js
@@ -1,88 +1,123 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const { confessId } = require('../config.json');
-const { airtable_API } = require('../config.json');
-const { airtableBase } = require('../config.json');
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: `${airtable_API}`}).base(airtableBase);
+const { SlashCommandBuilder } = require( '@discordjs/builders' );
+const { MessageEmbed, CommandInteraction, Client, MessageActionRow, MessageButton, MessageComponentInteraction, CollectorFilter, Message } = require( 'discord.js' );
+const { confessId } = require( '../config.json' );
+const { BlockLogs, Confessions } = require( '../database/database' );
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('confess')
-		.setDescription(`Make an anonymous (or signed) confession in the confessions channel!`)
-        .addStringOption(o => o
-			.setName(`type`)
-			.setDescription(`Please provide a type of confession:`)
-			.setRequired(true)
-			.addChoice(`Anonymous`, `1`)
-			.addChoice(`Signed`, `2`)
-			)
-        .addStringOption(o => o
-            .setName('confession')
-            .setDescription('Type out your confession here:')
-            .setRequired(true)
-            ),
-	async execute(interaction, client) {
+		.setName( 'confess' )
+		.setDescription( `Make an anonymous (or signed) confession in the confessions channel!` )
+		.addStringOption( o => o
+			.setName( `type` )
+			.setDescription( `Please provide a type of confession:` )
+			.setRequired( true )
+			.addChoice( `Anonymous`, `1` )
+			.addChoice( `Signed`, `2` )
+		)
+		.addStringOption( o => o
+			.setName( 'confession' )
+			.setDescription( 'Type out your confession here:' )
+			.setRequired( true )
+		),
+	/**
+	 * 
+	 * @param {CommandInteraction} interaction 
+	 * @param {Client} client 
+	 */
+	async execute ( interaction, client )
+	{
+		if ( !interaction.inCachedGuild() ) return
 
-		confessChannel = await client.channels.cache.find(c => c.id === `${confessId}`)
+		const confessChannel = await client.channels.fetch( confessId, { force: false, cache: true } )
+		if ( !confessChannel.isText() ) return
 
-		base('Blocks').select({
-			filterByFormula: `BlkID = ${interaction.user.id}`	
-		}).eachPage(function page(records, fetchNextPage) {
+		const blocks = await BlockLogs.findAll( { where: { usrID: interaction.user.id } } );
+		const blockedUser = blocks[ blocks.length - 1 ]
+		const action = blockedUser?.getDataValue( "action" );
 
-			try{
-				if (records[0]) {return interaction.editReply({content: `Sorry, but you have been blocked by staff from using this bot. Don't worry, nobody knows anything about the confession which you made, your privacy has been ensured.\nPlease go to <#812700884256686110> and create a ticket to get help.`})}
-				fetchNextPage();
-			} catch (err) {
-				console.error(err)
-			}
-		}, function done(error){
-			
-			const confType = interaction.options.getString('type');
-			const confession = interaction.options.getString('confession');
-			const icon = interaction.user.displayAvatarURL({dynamic: true});
+		if ( action === "unblock" || !action )
+		{
+			const confType = interaction.options.getString( 'type' );
+			const confession = interaction.options.getString( 'confession' );
+			const icon = interaction.user.displayAvatarURL( { dynamic: true } );
 
-			let embed = new MessageEmbed()
-				.setColor('RANDOM')
-				.setDescription(`\n\n${confession}\n`)
+			const embed = new MessageEmbed()
+				.setColor( 'RANDOM' )
+				.setDescription( `\n\n${ confession }\n` )
 				.setTimestamp()
-				.setTitle('Confession:');
+				.setTitle( 'Confession:' );
 
-			if (confType === '1') {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setCustomId( 'sendconfirm' )
+						.setEmoji( '✅' )
+						.setStyle( 'SUCCESS' )
+						.setLabel( 'Send' ),
+					new MessageButton()
+						.setCustomId( 'sendcancel' )
+						.setEmoji( '❎' )
+						.setStyle( 'PRIMARY' )
+						.setLabel( 'Cancel' )
+				)
 
-				embed.setAuthor({name: `Anon#0000`, iconURL: `${client.user.avatarURL()}`});
-				confessChannel.send({embeds: [embed]}).then(msg => newConfession(msg));
-				return interaction.editReply({content: `Your message has been sent! Please open a ticket in <#812700884256686110> if there are any issues!\n
-											We hope that this helps you in feeling better!`})
+			const filter = ( button ) => button.user.id === interaction.user.id
+			const reply = await interaction.editReply( { content: `You are about to send this confession. Send it? You have 60 seconds to decide.`, embeds: [ embed ], components: [ row ] } )
+			reply.awaitMessageComponent( {
+				filter,
+				time: 60000,
+				componentType: 'BUTTON'
+			} ).then( ( collected ) =>
+			{
+				if ( collected.customId === 'sendconfirm' )
+				{
 
-			} else if (confType === '2') {
-
-				embed.setAuthor({name: `${interaction.user.tag}`, iconURL: `${icon}`}).setFooter({text: `Please note that even though this confession was not anonymous, it does not allow anyone to send nasty stuff to ${interaction.user.username}. Read rule#2.`});
-				confessChannel.send({embeds: [embed]});
-				return interaction.editReply({content: `Your confession has been sent! Please open a ticket in <#812700884256686110> if there are any issues.\n
-											We hope that this helps you in feeling better!`});
-
-			}
-
-			async function newConfession(msg){
-				base('Confessions').create([
+					if ( confType === '1' )
 					{
-					"fields": {
-						"UsrID": `${interaction.user.id}`,
-						"MsgID": `${msg.id}`
+
+						embed.setAuthor( { name: `Anon#0000`, iconURL: `${ client.user.displayAvatarURL() }` } );
+						confessChannel.send( { embeds: [ embed ] } ).then( msg => newConfession( msg ) );
+						return collected.update( {
+							content: `Your message has been sent! Please open a ticket in <#812700884256686110> if there are any issues!\n
+															We hope that this helps you in feeling better!`, embeds: [], components: []
+						} )
+
+					} else if ( confType === '2' )
+					{
+
+						embed.setAuthor( { name: `${ interaction.user.tag }`, iconURL: `${ icon }` } ).setFooter( { text: `Please note that even though this confession was not anonymous, it does not allow anyone to send nasty stuff to ${ interaction.user.username }. Read rule#2.` } );
+						confessChannel.send( { embeds: [ embed ] } ).then( msg => newConfession( msg ) );;
+						return collected.update( {
+							content: `Your confession has been sent! Please open a ticket in <#812700884256686110> if there are any issues.\n
+															We hope that this helps you in feeling better!`, embeds: [], components: []
+						} );
 					}
-					}
-				], function(err, records) {
-					if (err) {
-					console.error(err);
-					return;
-					}
-				})
+				} else
+				{
+					return collected.update( { content: `Confession was not sent. For your reference, here is your confession:\n----------------\n${ confession }`, embeds: [], components: [] } )
+				}
+			} ).catch( async () =>
+			{
+				interaction.editReply( { content: `You did not click the buttons in time. If you wish to try again, here is your confession:\n----------------\n${ confession }`, embeds: [], components: [] } )
+			} )
+
+			/**
+			 * 
+			 * @param {Message} msg 
+			 */
+			async function newConfession ( msg )
+			{
+				Confessions.create( {
+					usrID: interaction.user.id,
+					msgID: msg.id
+				} ).then( () => { } ).catch( ( err ) =>
+				{
+					console.error( err )
+				} );
 			}
-
-		})
-
-		
-
+		} else if ( action === "block" )
+		{
+			return interaction.editReply( { content: `Sorry, but you have been blocked by staff from using this bot. Don't worry, nobody knows anything about the confession which you made, your privacy has been ensured.\nPlease go to <#812700884256686110> and create a ticket to get help.` } )
+		}
 	},
 };
