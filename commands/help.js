ab5e5f4beba32c36e554669d3adaff25b4fad520
MatricalDefunkt <pramitsmusic@gmail.com>
2022-06-05 00:35:37 +0530

    Added title to the main help screen.

--- commands/help.js
+++ commands/help.js
@@ -1,40 +1,47 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder } = require( '@discordjs/builders' );
+const { MessageEmbed, MessageActionRow, MessageButton, CommandInteraction, Client } = require( 'discord.js' );
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('confesshelp')
-		.setDescription(`Provides some description about the bot, its features and commands.`),
-	async execute(interaction, client) {
+		.setName( 'confesshelp' )
+		.setDescription( `Provides some description about the bot, its features and commands.` ),
+	/**
+	 * 
+	 * @param {CommandInteraction} interaction 
+	 * @param {Client} client 
+	 */
+	async execute ( interaction, client )
+	{
 
-        helpEmbed = new MessageEmbed()
-        row = new MessageActionRow().addComponents(
+		const helpEmbed = new MessageEmbed()
+		const row = new MessageActionRow().addComponents(
 			new MessageButton()
-				.setCustomId('privacy')
-				.setEmoji('859242026620813343')
-				.setLabel('Privacy')
-				.setStyle('PRIMARY'),
+				.setCustomId( 'privacy' )
+				.setEmoji( '859242026620813343' )
+				.setLabel( 'Privacy' )
+				.setStyle( 'PRIMARY' ),
 			new MessageButton()
-				.setCustomId('commands')
-				.setEmoji('🤖')
-				.setLabel('Commands')
-				.setStyle('PRIMARY'),
-            new MessageButton()
-                .setCustomId('home')
-                .setEmoji('🏠')
-                .setLabel('Home')
-                .setStyle('PRIMARY')
-                .setDisabled(true)
+				.setCustomId( 'commands' )
+				.setEmoji( '🤖' )
+				.setLabel( 'Commands' )
+				.setStyle( 'PRIMARY' ),
+			new MessageButton()
+				.setCustomId( 'home' )
+				.setEmoji( '🏠' )
+				.setLabel( 'Home' )
+				.setStyle( 'PRIMARY' )
+				.setDisabled( true )
 		);
-        helpEmbed.setAuthor({name: `${interaction.user.tag}`, iconURL:`${interaction.user.avatarURL()}`})
-        helpEmbed.setDescription(`
+		helpEmbed.setAuthor( { name: `${ interaction.user.tag }`, iconURL: `${ interaction.user.avatarURL() }` } )
+		helpEmbed.setDescription( `
         Hey there! Looks like you asked for some help! Well, glad you did so.
         Here is some information that might help you. As always, if this does not suffice, you can always open a ticket in <#812700884256686110>.
 
         To get help about the slash commands, click on "Commands", and for help with privacy, click on "Privacy"
         `)
+		helpEmbed.setTitle( "Help:" )
 
-        interaction.editReply({embeds:[helpEmbed], components: [row]})
+		interaction.editReply( { embeds: [ helpEmbed ], components: [ row ] } )
 
 	},
 };
