1823bcca612de63f856e36f7f665a60c620aeae3
MatricalDefunkt <pramitsmusic@gmail.com>
2022-06-05 00:35:12 +0530

    Added titles to embeds

--- buttons/privacy.js
+++ buttons/privacy.js
@@ -1,46 +1,45 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageButton } = require( "discord.js" )
 
 module.exports = {
     customId: 'privacy',
 
-    async execute(interaction, client) {
+    async execute ( interaction, client )
+    {
 
-        privacyEmbed = new MessageEmbed();
-        row = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setCustomId('privacy')
-				.setEmoji('859242026620813343')
-				.setLabel('Privacy')
-				.setStyle('PRIMARY')
-                .setDisabled(true),
-			new MessageButton()
-				.setCustomId('commands')
-				.setEmoji('🤖')
-				.setLabel('Commands')
-				.setStyle('PRIMARY'),
+        const privacyEmbed = new MessageEmbed();
+        const row = new MessageActionRow().addComponents(
             new MessageButton()
-                .setCustomId('home')
-                .setEmoji('🏠')
-                .setLabel('Home')
-                .setStyle('PRIMARY')
-		);
+                .setCustomId( 'privacy' )
+                .setEmoji( '859242026620813343' )
+                .setLabel( 'Privacy' )
+                .setStyle( 'PRIMARY' )
+                .setDisabled( true ),
+            new MessageButton()
+                .setCustomId( 'commands' )
+                .setEmoji( '🤖' )
+                .setLabel( 'Commands' )
+                .setStyle( 'PRIMARY' ),
+            new MessageButton()
+                .setCustomId( 'home' )
+                .setEmoji( '🏠' )
+                .setLabel( 'Home' )
+                .setStyle( 'PRIMARY' )
+        );
 
-        privacyEmbed.setAuthor({name: `${interaction.user.tag}`, iconURL:`${interaction.user.avatarURL()}`})
-        privacyEmbed.setDescription(
-        `
+        privacyEmbed.setAuthor( { name: `${ interaction.user.tag }`, iconURL: `${ interaction.user.displayAvatarURL( { dynamic: true } ) }` } )
+        privacyEmbed.setDescription( `
+            This bot ensures your privacy, as well as allows for security.
 
-Privacy:
-This bot ensures your privacy, as well as allows for security.
+            The message ID of the final confession is stored where it is linked to your user ID, but this is only for security and prevention of misuse.
 
-The message ID of the final confession is stored where it is linked to your user ID, but this is only for security and prevention of misuse.
+            Note: Neither staff, nor any other human being will ever be able to read this information. There is no premium version of this bot which allows for the same.
 
-Note: Neither staff, nor any other human being will ever be able to read this information. There is no premium version of this bot which allows for the same.
-
-The bot is fully open source and available at https://github.com/MatricalDefunkt/Confessions-Bot
+            The bot is fully open source and available on [GitHub](https://github.com/MatricalDefunkt/Confessions-Bot) 
 
         `)
+        privacyEmbed.setTitle( "Privacy:" )
 
-        interaction.update({embeds:[privacyEmbed], components:[row]})
+        interaction.update( { embeds: [ privacyEmbed ], components: [ row ] } )
 
     }
 }
