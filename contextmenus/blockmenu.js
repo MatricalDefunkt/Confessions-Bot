3b0ec17e85517224e37be4788b2ce1540821ffd8
MatricalDefunkt <pramitsmusic@gmail.com>
2022-06-23 00:11:51 +0530

    Added a modal in the blocking interaction where the modal asks for a reason for blocking

--- contextmenus/blockmenu.js
+++ contextmenus/blockmenu.js
@@ -1,5 +1,5 @@
 const { ContextMenuCommandBuilder } = require( '@discordjs/builders' );
-const { MessageEmbed, MessageActionRow, MessageButton, MessageContextMenuInteraction } = require( 'discord.js' );
+const { MessageEmbed, MessageActionRow, MessageButton, MessageContextMenuInteraction, Modal, TextInputComponent } = require( 'discord.js' );
 const { staffRoleId, confessId } = require( '../config.json' );
 const { BlockLogs, Confessions } = require( '../database/database' );
 
@@ -14,6 +14,8 @@ module.exports = {
 	 */
 	async execute ( interaction )
 	{
+		const message = await interaction.channel.messages.fetch( `${ interaction.targetId }`, { force: false, cache: true } );
+
 		if ( !interaction.inCachedGuild() ) return
 
 		if ( !interaction.member.roles.resolve( staffRoleId ) )
@@ -22,9 +24,8 @@ module.exports = {
 			return interaction.editReply( { content: `This command is reserved for ${ staffRole } only.` } )
 		}
 
-		if ( interaction.channelId !== confessId ) return interaction.editReply( { content: `\`\`\`diff\n- The selected message is not a confession.\`\`\`` } )
+		if ( interaction.channelId !== confessId || message.embeds.length !== 1 ) return interaction.editReply( { content: `\`\`\`diff\n- The selected message is not a confession.\`\`\`` } )
 
-		const message = await interaction.channel.messages.fetch( `${ interaction.targetId }` );
 		const row = new MessageActionRow().addComponents(
 			new MessageButton()
 				.setCustomId( 'yes' )
@@ -37,11 +38,29 @@ module.exports = {
 				.setLabel( 'Cancel' )
 				.setStyle( 'PRIMARY' ),
 		);
+
+		const modal = new Modal()
+			.setCustomId( "reasonModal" )
+			.setTitle( "Block Reason:" )
+
+		const reasonInputComponent =
+			new TextInputComponent()
+				.setStyle( "PARAGRAPH" )
+				.setCustomId( "reason" )
+				.setLabel( "Reason for blocking." )
+				.setPlaceholder( "Under 512 characters..." )
+				.setMinLength( 16 )
+				.setMaxLength( 235 )
+				.setRequired( true )
+
+		const reasonInputRow = new MessageActionRow().addComponents( reasonInputComponent )
+		modal.addComponents( reasonInputRow )
+
 		const reply = await interaction.editReply( {
 			embeds: [ new MessageEmbed()
 				.setAuthor( { name: `${ interaction.user.tag }`, iconURL: `${ interaction.user.displayAvatarURL( { dynamic: true } ) }` } )
 				.setTitle( `Message link` )
-				.setURL( `https://discord.com/channels/${ interaction.guild.id }/${ interaction.channel.id }/${ message.id }` )
+				.setURL( message.url )
 				.setDescription( `The confessee is about to be blocked. Proceed? You have 5 minuted to decide.` )
 			],
 			components: [ row ]
@@ -49,48 +68,71 @@ module.exports = {
 		reply.awaitMessageComponent( { componentType: "BUTTON", time: 300_000 } )
 			.then( async ( button ) =>
 			{
-				await button.deferUpdate()
 				if ( button.customId === "yes" )
 				{
-					const confession = await Confessions.findByPk( message.id )
-					if ( !confession )
+					await button.showModal( modal )
+					await button.editReply( { components: [] } )
+					button.awaitModalSubmit( { time: 300_000 } ).then( async ( modalInteraction ) =>
 					{
-						message.delete()
-						return button.editReply( {
-							content: `\`\`\`diff\n- Message was not found in the database, but this message has been deleted for now.\n- This is an error. Please contact Matrical ASAP.\`\`\``, embeds: [], components: []
-						} )
-					} else
-					{
-						const usrID = confession.getDataValue( "usrID" )
-						const blockCount = await BlockLogs.count( { where: { usrID, action: "block" } } )
-						const previousBlocks = await BlockLogs.findAll( { where: { usrID } } )
+						await modalInteraction.deferUpdate()
 
-						if ( previousBlocks[ previousBlocks - 1 ]?.getDataValue( "action" ) === "block" ) return button.editReply( { content: `Confessee is already blocked.`, components: [], embeds: [] } )
-
-						BlockLogs.create( {
-							usrID: confession.getDataValue( "usrID" ),
-							action: "block",
-							modID: interaction.user.id
-						} ).then( ( result ) =>
+						const reason = modalInteraction.components[ 0 ].components[ 0 ].value
+						const confession = await Confessions.findByPk( message.id )
+						if ( !confession )
 						{
-							button.editReply( {
-								content: `\`\`\`diff\n+ Confessee has been blocked from making any new confessions.${ ( blockCount > 0 ) ? `\nConfessee has been previously blocked ${ blockCount } time(s).` : `` }\`\`\``, embeds: [], components: []
-							} ).then( () => message.delete() )
-						} ).catch( async ( err ) =>
-						{
-							return button.editReply( {
-								content: `There was an error. Please contact Matrical ASAP.`, embeds: [], components: []
+							message.delete()
+							return modalInteraction.editReply( {
+								content: `\`\`\`diff\n- Message was not found in the database, but this message has been deleted for now.\n- This is an error. Please contact Matrical ASAP.\`\`\``, embeds: [], components: []
 							} )
-						} );
-					}
+						} else
+						{
+							const usrID = confession.usrID
+							const blockCount = await BlockLogs.count( { where: { usrID, action: "block" } } )
+							const previousBlocks = await BlockLogs.findAll( { where: { usrID } } )
+
+							if ( previousBlocks[ previousBlocks - 1 ]?.getDataValue( "action" ) === "block" ) return modalInteraction.editReply( { content: `Confessee is already blocked.`, components: [], embeds: [] } )
+
+							await BlockLogs.create( {
+								usrID: usrID,
+								action: "block",
+								modID: interaction.user.id,
+								reason
+							} ).then( ( result ) =>
+							{
+								modalInteraction.editReply( {
+									content: `\`\`\`diff\n+ Confessee has been blocked from making any new confessions for the reason: \`${ reason }\`.${ ( blockCount > 0 ) ? `\nConfessee has been previously blocked ${ blockCount } time(s).` : `` }\`\`\``, embeds: [], components: []
+								} ).then( () => message.delete() )
+							} ).catch( async ( err ) =>
+							{
+								return modalInteraction.editReply( {
+									content: `There was an error. Please contact Matrical ASAP.`, embeds: [], components: []
+								} )
+							} );
+
+						}
+					} ).catch( err =>
+					{
+						if ( err?.code === "INTERACTION_COLLECTOR_ERROR" )
+							return interaction.editReply( { content: `You did not submit modal in time. Please try again, if you wish to do so.`, embeds: [], components: [] } )
+						else
+						{
+							console.error( err )
+							return interaction.editReply( { content: `There was an error. Please contact Matrical ASAP.`, embeds: [], components: [] } )
+						}
+					} )
 				} else
 				{
 					button.editReply( { content: `Cancelled.`, components: [], embeds: [] } )
 				}
 			} ).catch( ( err ) =>
 			{
-				console.error( err )
-				return interaction.editReply( { content: `You did not click the buttons in time. Please try again, if you wish to do so.` } )
+				if ( err?.code === "INTERACTION_COLLECTOR_ERROR" )
+					return interaction.editReply( { content: `You did not click the buttons in time. Please try again, if you wish to do so.`, embeds: [], components: [] } )
+				else
+				{
+					console.error( err )
+					return interaction.editReply( { content: `There was an error. Please contact Matrical ASAP.`, embeds: [], components: [] } )
+				}
 			} );
 	},
 };
