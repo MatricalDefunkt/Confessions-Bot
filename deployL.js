af13b96d4c7966b7ad4f2a3bd2a19995fb662fb8
MatricalDefunkt <pramitsmusic@gmail.com>
2022-07-24 21:49:13 +0530

    Prettified file, and changed API version to V10

--- deployL.js
+++ deployL.js
@@ -1,39 +1,44 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { token } = require('./config.json');
-const fs = require('fs');
-const { guildId } = require(`./config.json`);
-const { clientId } = require(`./config.json`)
+const { REST } = require( '@discordjs/rest' );
+const { Routes } = require( 'discord-api-types/v9' );
+const { token } = require( './config.json' );
+const fs = require( 'fs' );
+const { guildId } = require( `./config.json` );
+const { clientId } = require( `./config.json` )
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const contextFiles = fs.readdirSync('./contextmenus').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync( './commands' ).filter( file => file.endsWith( '.js' ) );
+const contextFiles = fs.readdirSync( './contextmenus' ).filter( file => file.endsWith( '.js' ) );
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+for ( const file of commandFiles )
+{
+	const command = require( `./commands/${ file }` );
+	commands.push( command.data.toJSON() );
 }
 
-for (const file of contextFiles) {
-	const contextMenu = require(`./contextmenus/${file}`);
-	commands.push(contextMenu.data.toJSON());
+for ( const file of contextFiles )
+{
+	const contextMenu = require( `./contextmenus/${ file }` );
+	commands.push( contextMenu.data.toJSON() );
 }
 
 
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST( { version: '10' } ).setToken( token );
 
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands. (**LOCAL ONLY**)');
+( async () =>
+{
+	try
+	{
+		console.log( 'Started refreshing application (/) commands. (**LOCAL ONLY**)' );
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands( clientId, guildId ),
 			{ body: commands },
 		);
 
-		console.log('Successfully reloaded application (/) commands. (**LOCAL ONLY**)');
-	} catch (error) {
-		console.error(error);
+		console.log( 'Successfully reloaded application (/) commands. (**LOCAL ONLY**)' );
+	} catch ( error )
+	{
+		console.error( error );
 	}
-})();
+} )();
